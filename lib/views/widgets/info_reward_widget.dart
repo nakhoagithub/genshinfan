6d86b06a4ed933c926417de72c9c0176a3597a64
Anh Khoa <dev.nakhoa@gmail.com>
2023-04-24 23:57:22 +0700

    Update version

--- lib/views/widgets/info_reward_widget.dart
+++ lib/views/widgets/info_reward_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:genshinfan/controllers/resource_controller.dart';
 import 'package:genshinfan/objects/resource.dart';
